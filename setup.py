7aa8626485c24f88e0e24a796aa86d53e0c0091c
gkanfer <gkanfer007@gmail.com>
2024-03-07 08:35:44 -0800

    0.0.21

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="AIPySdeAnalyzer",
-    version="0.0.2",
+    version="0.0.21",
     install_requires=required,
     packages=find_namespace_packages(where='src'),
     package_dir={'': 'src'},
