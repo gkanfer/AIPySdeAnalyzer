b7cfb0d48a96a5618694c6555385e6863f7ab9dd
gkanfer <gkanfer007@gmail.com>
2024-03-21 04:55:31 -0700

    fix simulation h0 generation

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 from aipys_analyse.simulation.runSimulation import runSimulation
 import pandas as pd
 def main():
-    df = runSimulation(targetNum = 3, geneNum = 300, effectSgRNA = 4,getData = True, mu = 20, a = 1.2,low = 1, high = 1,size = 10_000,FalseLimits = (0.6,0.9),ObservationNum = (10,3))
+    df = runSimulation(targetNum = 3, geneNum = 2000, effectSgRNA = 4,getData = True, mu = 20, a = 1.2,low = 1, high = 3,size = 100_000,FalseLimits = (0.6,0.9),ObservationNum = (3000,300))
     print(df)
 if __name__ == "__main__":
     main()
